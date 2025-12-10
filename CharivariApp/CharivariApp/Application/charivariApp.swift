5fb9e432581f28800dc0fd70fe0ac03a2a243aa3
John Doe <200684522+utilisateurID@users.noreply.github.com>
2025-12-10 09:51:01 -0500

    Ajout - Structure du projet

---
+++ CharivariApp/CharivariApp/Application/charivariApp.swift
@@ -0,0 +1,16 @@
+//
+//  charivariApp.swift
+//
+//  Auteur : Louis-Philippe Boudreau
+//
+
+import SwiftUI
+
+@main
+struct charivariApp: App {
+    var body: some Scene {
+        WindowGroup {
+
+        }
+    }
+}
